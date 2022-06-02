a895ba2ae92e34660b0b9d4a7bb9a974b609b517
Kosta Kazakov <kostakazakoff@gmail.com>
2022-06-02 13:07:42 +0300

    Changes in number_filter_2.py

--- lab_lists_basics/numbers_filter_2.py
+++ lab_lists_basics/numbers_filter_2.py
@@ -3,14 +3,18 @@ integers = [int(input()) for _ in range(integers_count)]
 command = input()
 result = []
 
-for i in integers:
-    filter_integers = (
-        (command == 'even' and i % 2 == 0),
-        (command == 'odd' and i % 2 != 0),
-        (command == 'positive' and i >= 0),
-        (command == 'negative' and i < 0)
-    )
-    if any(filter_integers):
-        result.append(i)
 
-print(result)
+def filter_int(definition, list_of_integers):
+    for i in list_of_integers:
+        filter_integers = (
+            (definition == 'even' and i % 2 == 0),
+            (definition == 'odd' and i % 2 != 0),
+            (definition == 'positive' and i >= 0),
+            (definition == 'negative' and i < 0)
+        )
+        if any(filter_integers):
+            result.append(i)
+    return result
+
+
+print(filter_int(command, integers))
