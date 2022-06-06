7717635f9a6491afdaa5b918f6869af4b0a816c2
Kosta Kazakov <kostakazakoff@gmail.com>
2022-06-06 14:53:19 +0300

    Update messaging_2.py

--- list_basics_more_exercises/messaging_2.py
+++ list_basics_more_exercises/messaging_2.py
@@ -3,12 +3,20 @@ source = input()
 
 
 def message_encode(strings_of_numbers, source_message):
+    # creating a list of simbols, needed for the task:
     source_message = [x for x in source_message]
     message = ''
+    
+    # iterating to extract the codes from the numbers list:
     for code in strings_of_numbers:
+        # taking the sum of code integers
+        # it's the searched index of the simbol in the source message list:
         i = sum(int(x) for x in code)
+        # if the end of the source message list is exceeded,
+        # start from the first index (using a division by remainder):
         if i > len(source_message):
             i %= len(source_message)
+        # appending the message string, removing the value in the source message:
         message += source_message.pop(i)
     return message
 
