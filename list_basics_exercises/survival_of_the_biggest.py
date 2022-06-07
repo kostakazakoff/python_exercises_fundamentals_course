b5a0975fed0b8f0f2e117d1fe918386d97a81a19
Kosta Kazakov <kostakazakoff@gmail.com>
2022-06-07 09:11:16 +0300

    Changed variable name

--- list_basics_exercises/survival_of_the_biggest.py
+++ list_basics_exercises/survival_of_the_biggest.py
@@ -1,9 +1,9 @@
 numbers = list(map(int, input().split()))
-remove_count = int(input())
+removes_count = int(input())
 
 numbers_copy = numbers.copy()
 numbers_copy.sort()
-edited_list = numbers_copy[remove_count:]
+edited_list = numbers_copy[removes_count:]
 numbers = [str(x) for x in numbers if x in edited_list]
 numbers = ', '.join(numbers)
 print(numbers)
