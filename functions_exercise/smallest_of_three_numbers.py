c4739e3d437ce22aa6719b3a0c1ee2b5723fecbb
Kosta Kazakov <kostakazakoff@gmail.com>
2022-06-12 16:38:51 +0300

    Edit

--- functions_exercise/smallest_of_three_numbers.py
+++ functions_exercise/smallest_of_three_numbers.py
@@ -1,17 +1,11 @@
-import sys
+def min_integer(int_1, int_2, int_3):
+    numbers = (int_1, int_2, int_3)
+    min_int = min(numbers)
+    return min_int
+
 
 x = int(input())
 y = int(input())
 z = int(input())
-
-
-def min_integer(n1, n2, n3):
-    numbers = [n1, n2, n3]
-    min_int = sys.maxsize
-    for i in range(3):
-        if numbers[i] < min_int:
-            min_int = numbers[i]
-    return min_int
-
 
 print(min_integer(x, y, z))
