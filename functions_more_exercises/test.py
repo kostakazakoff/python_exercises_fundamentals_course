71bb1a33fbb0f84027047021696a745693cb959b
Kosta Kazakov <kostakazakoff@gmail.com>
2022-06-13 23:31:24 +0300

    Delete test.py

--- functions_more_exercises/test.py
+++
@@ -1,8 +0,0 @@
-all_points = []
-for points in range(4):
-    coordinates = []
-    for point in range(2):
-        xy = float(input())
-        coordinates.append(xy)
-    all_points.append(coordinates)
-print(all_points)
